77835b1d5dae9d2935ccfc3d4c3013f1aa03be31
whl36512 <whl36512@gmail.com>
2018-12-05 18:47:13 +0000

    host 127.0.0.1

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub static PG_PORT  : &str ="5432";
-pub static PG_HOST  : &str ="10.1.0.110" ;
+pub static PG_HOST  : &str ="127.0.0.1" ;
 pub static PG_USER  : &str ="ride"    ;
 pub static PG_PASSWD    : &str ="ride" ;
 pub static PG_DATABASE  : &str ="ride" ;
